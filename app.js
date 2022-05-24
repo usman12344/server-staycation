a17bacf59399c05c34e7b0fdbbc48e2762ce7006
usman <usmanabdat6@gmail.com>
2022-05-24 11:49:00 +0700

    cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const cors = require('cors');
 //import mongoose
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://usman:usmanabdat@cluster0.taibl.mongodb.net/db_staycation?retryWrites=true&w=majority', {
@@ -22,6 +23,8 @@ const adminRouter = require('./routes/admin');
 const apiRouter = require('./routes/api');
 
 var app = express();
+
+app.use(cors());
  
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
